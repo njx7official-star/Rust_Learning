f1605f89422fe6deb8b8bfe03dfb10312002746a
NJ7 <nirajv172717@gmail.com>
2026-06-09 16:30:59 +0530

    Make greeting matching case-insensitive : Fix greeting match by using lowercase patterns Rust match compares strings exactly. Since user input is converted with to_lowercase(), match patterns must also be lowercase.

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
         .expect("Failed to read input");
 
     // use of match expression to pattern match against variable "name"
-    match name.trim() {
-        "Good Bye" => println!("Sorry to see you go."),
-        "Hello" => println!("Hi, nice to meet you!"),
-        "How are you?" => println!("I'm doing well!"),
-        "Good morning" => println!("Whats up nj ?? Good Morning"),
-        "Good evening" => println!("Good evening..! , Nj"),
+    match name.trim().to_lowercase().as_str() {
+        "good bye" => println!("Sorry to see you go."),
+        "hello" => println!("Hi, nice to meet you!"),
+        "how are you?" => println!("I'm doing well!"),
+        "good morning" => println!("Whats up nj ?? Good Morning"),
+        "good evening" => println!("Good evening..! , Nj"),
         _ => println!("I can't find a greeting, good bye."),
     }
 }
