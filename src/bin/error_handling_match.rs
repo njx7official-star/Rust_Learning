7b8fc89dae07a075bd0b663899ed9a42da430026
NJ7 <nirajv172717@gmail.com>
2026-06-10 01:54:00 +0530

    Add file writing with match error handling

--- src/bin/error_handling_match.rs
+++ src/bin/error_handling_match.rs
@@ -1,6 +1,15 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-
+use std::error;
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, BufWriter, Write};
+fn writer(file: File) {
+    let mut writer = BufWriter::new(file);
+    match writeln!(writer, "Nj new line added") {
+        Ok(()) => {}
+        Err(error) => {
+            panic!("Error WRITING TO FILE : {}", error)
+        }
+    }
+}
 fn main() {
     let file = File::open("permission_denied_file.txt");
     let file = match file {
@@ -25,6 +34,18 @@ fn main() {
             Err(error) => {
                 panic!("Error reading line: {}", error)
             }
+        }
+    }
+    let writer_file = OpenOptions::new()
+        .write(true)
+        .open("permission_denied_file.txt");
+    match writer_file {
+        Ok(file) => {
+            println!("File Found and Opening");
+            writer(file);
+        }
+        Err(error) => {
+            panic!("Error while opening for editing {}", error);
         }
     }
 }
