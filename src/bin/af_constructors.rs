a4a95e472b6749b026502f88fb26ab2d279b154a
NJ7 <nirajv172717@gmail.com>
2026-06-10 17:14:44 +0530

    Modified the code to take username from email split

---
+++ src/bin/af_constructors.rs
@@ -0,0 +1,48 @@
+struct User {
+    username: String,
+    email: String,
+    uri: String,
+    active: bool,
+}
+
+impl User {
+    fn new(username: String, email: String, uri: String) -> Self {
+        Self {
+            username,
+            email,
+            uri,
+            active: true,
+        }
+    }
+    fn deactivate(&mut self) {
+        self.active = false;
+    }
+    fn from_email(&mut self) {
+        let user_name_femail = self
+            .email
+            .split_once("@")
+            .map(|(name, _)| name.to_string())
+            .unwrap_or_else(|| self.email.clone());
+        self.username = user_name_femail;
+    }
+}
+
+fn main() {
+    let mut new_user = User::new(
+        String::from("alfredodeza123"),
+        String::from("alfreodeza@example.com"),
+        String::from("https://alfredodeza.com"),
+    );
+    println!("Hello, {}!", new_user.username);
+    println!(
+        "Account {} status is: {}",
+        new_user.username, new_user.active
+    );
+    new_user.deactivate();
+    println!(
+        "Account {} status is: {}",
+        new_user.username, new_user.active
+    );
+    new_user.from_email();
+    println!("new user name {}",new_user.username);
+}
