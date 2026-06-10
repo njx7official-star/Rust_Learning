2ddd4bce75465951eed29ba0c38cad04787bc0b8
NJ7 <nirajv172717@gmail.com>
2026-06-11 00:10:38 +0530

    Task 2 : Completed : Extend the program by implementing a function that takes a file size in bytes as input and returns the file size representation in the largest possible unit.

--- src/bin/match-enum.rs
+++ src/bin/match-enum.rs
@@ -24,7 +24,20 @@ fn format_size(size: u64) -> String {
     }
 }
 
+fn rep_largest(s: u64) -> String {
+    let files_in_bytes = match s {
+        0..=1023 => format!("{} bytes", s),
+        1024..=1_048_575 => format!("{:.2} KB", s as f64 / 1024 as f64),
+        1_048_576..=1_073_741_823 => format!("{:.2} MB", s as f64 / 1_048_576 as f64),
+        1_073_741_824..=1_099_511_627_774 => format!("{:.2} GB", s as f64 / 1_073_741_824 as f64),
+        _ => format!("{:.2} TB", s as f64 / 1_099_511_627_776.0 as f64),
+    };
+    files_in_bytes
+}
+
 fn main() {
     let result = format_size(60000888837399);
-    println!("{}", result)
+    let largest = rep_largest(1_048_57611);
+    println!("{}", result);
+    println!("{}", largest);
 }
