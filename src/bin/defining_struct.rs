be9d69325e44e5ff4dc1a3569fe9c4dc30e7b78b
NJ7 <nirajv172717@gmail.com>
2026-06-10 16:24:50 +0530

    Implemented a method to concatinating strings : method used format!

--- src/bin/defining_struct.rs
+++ src/bin/defining_struct.rs
@@ -8,14 +8,14 @@ struct Person {
 }
 
 fn main() {
-    println!(
-        "{:?}",
-        Person {
-            first_name: "John".to_string(),
-            last_name: "Doe".to_string(),
-            email: "nirajv.official@gmail.com".to_string(),
-            ph_no: 9526256200,
-            age: 25,
-        }
-    );
+    let nj = Person {
+        first_name: "John".to_string(),
+        last_name: "Doe".to_string(),
+        email: "nirajv.official@gmail.com".to_string(),
+        ph_no: 9526256200,
+        age: 25,
+    };
+    println!("{:?}", nj);
+    let full_name = format!("{} {}", nj.first_name, nj.last_name);
+    println!("Full name : {}", full_name);
 }
